0a4aa71286f4a63fbe9ced451b789c013c3c47ff
dhejl <daniel.hejl@jetminds.com>
2012-05-25 13:19:25 +0200

    node.js server port according to ENV variable

--- web.js
+++ web.js
@@ -1,8 +1,10 @@
 var staticServer = require('node-static');
 var file = new staticServer.Server();
+var port = process.env.PORT || 3000;
+
 
 require('http').createServer(function (request, response) {
     request.addListener('end', function () {
         file.serve(request, response);
     });
-}).listen(8080);
+}).listen(port);
