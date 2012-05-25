ce5ffffac1dd9bde30c640839c8c0654c6d5229d
dhejl <daniel.hejl@jetminds.com>
2012-05-25 13:10:34 +0200

    added node static server for heroku deployment

---
+++ web.js
@@ -0,0 +1,8 @@
+var staticServer = require('node-static');
+var file = new staticServer.Server();
+
+require('http').createServer(function (request, response) {
+    request.addListener('end', function () {
+        file.serve(request, response);
+    });
+}).listen(8080);
